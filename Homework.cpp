a83b4db17051306ee747674e5860d7b2795f31a3
tom lawless <lawlessptom@gmail.com>
2020-12-09 14:22:16 -0500

    Create Homework.cpp

---
+++ Homework.cpp
@@ -0,0 +1,21 @@
+//
+//  Homework.cpp
+//  
+//
+//  Created by Thomas Lawless III on 12/9/20.
+//
+#include <iostream>
+
+class Homework{
+private:
+    String name;
+    //dateTime dueDate;
+    //dateTime timeComplete;
+    //dateTime dateComplete;
+    // need to make sure dateTime will be able to be searched and updated easily
+    double Grade;
+    bool completed = false;
+    
+//getters and setters needed here
+
+}
