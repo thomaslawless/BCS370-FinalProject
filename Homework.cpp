f45d6d17dbac86e9b9361301e2f303f1a1540403
tom lawless <lawlessptom@gmail.com>
2020-12-10 13:59:21 -0500

    Update Homework.cpp
    adding setters

--- Homework.cpp
+++ Homework.cpp
@@ -5,8 +5,18 @@
 //  Created by Thomas Lawless III on 12/9/20.
 //
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Homework{
+public:
+    //setters
+    void setName(String assignName);
+    void setDueDate();
+    void setTimeComplete();
+    void setDateComplete();
+    void setGrade();
+    bool setCompleteAssign();
 private:
     String name;
     //dateTime dueDate;
@@ -15,7 +25,37 @@ private:
     // need to make sure dateTime will be able to be searched and updated easily
     double Grade;
     bool completed = false;
+        
+    //set the assignment name;
+    void Homework::setName(String studentName){
+        name = studentName;
+    }
     
-//getters and setters needed here
-
+    //set the due date
+    void Homework::setDueDate(){
+        
+    }
+    
+    //set the complete time
+    void Homework::setTimeComplete(){
+        
+    }
+    
+    //set the date complete
+    void Homework::setDateComplete(){
+        
+    }
+    
+    //set the grade
+    void Homework::setGrade(){
+        
+    }
+    
+    //set if assignment was completed
+    void Homework::setCompleteAssign(){
+        
+    }
+  
+    
 }
+
