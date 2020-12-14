60b8209bf0fae44d5a4c93c4e6e096862bdfa112
tom lawless <lawlessptom@gmail.com>
2020-12-14 18:09:23 -0500

    update main

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 int main()
 {
+    val schedule -= new HomeworkSchedule("test","test");
   
     std::cout << "Welcome to Homework Manager";
     std::cout << "Please select a function:";
@@ -22,5 +23,15 @@ int main()
         std::cout << "Please enter the A. Name of the Assignment B. Due Date";
         cin >> name;
         cin >> duedate;
+        schedule.addHW(name, dueDate);
+    }if (selection == 2) {
+        std::cout << "Please enter the A. Name of the Assignment B. DueDate";
+        cin >> name;
+        cin >> duedate;
+        schedule.getHW(name, duedate);
+    }if (input == 3){
+        std::cout << "The total number of assignments is" + schedule.getAssignments();
+    }if (input == 4){
+        return 3;
     }
 }
