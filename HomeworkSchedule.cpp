81eee1eb18f4b70b56f3cf3605c9ffd7d944be6b
tom lawless <lawlessptom@gmail.com>
2020-12-14 16:22:58 -0500

    adding search function

--- HomeworkSchedule.cpp
+++ HomeworkSchedule.cpp
@@ -5,14 +5,18 @@ using namespace std;
 
 class HomeworkSchedule: public Homework{
     map<Homework, int> assignmentsMap;
-
+    public int id = rand() % 100;
+    std::map<Homework,int>::iterator it;
+    
 //method to add a new assignment to the map
     public void addAssignment(string name, String dueDate){
-        Homework assignmentsMap = new Homework(name, dueDate, null, null, null, false)
+        Homework newAssignment = new Homework(name, dueDate, null, null, null, false)
+        assignmentsMap.insert(newAssignment, id);
     }
+    
 // update assignment
     public void updateAssignment(){
-
+        
     }
 
 //complete an assignment
@@ -20,8 +24,19 @@ class HomeworkSchedule: public Homework{
         h.setCompleteAssign(true);
 
     }
+    
 // list total  number of assignments
     public void getAssignments(){
         return assignmentsMap.size();
+    }
+    
+    public void searchCourse(String search){
+        it = assignmentsMap.find(search);
+        
+        if (it == assignmentsMap.end()){
+            cout << "Assignment does not exist";
+        }else {
+            cout << "The assignment exists";
+        }
     }
 }
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+//
+//  Main.cpp
+//  
+//
+//  Created by Thomas Lawless III on 12/14/20.
+//
+
+#include <stdio.h>
